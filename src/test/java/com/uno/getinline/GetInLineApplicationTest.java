6bdcc438c8cfd4decbf45f61f238b03fe6ca38eb
jason <jason@i-on.net>
2021-12-22 07:18:47 +0900

    initializing project

---
+++ src/test/java/com/uno/getinline/GetInLineApplicationTest.java
@@ -0,0 +1,14 @@
+package com.uno.getinline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class GetInLineApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
